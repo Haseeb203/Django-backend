3e4c8f67a369a2fa47e784ce044540e3e9e91d95
Haseeb203 <haseebsheikh203@gmail.com>
2025-05-16 01:20:35 +0500

    Added next practice task

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render ,get_object_or_404
+from .models import Post
 
 # Create your views here.
+
+posts = [
+   
+]
 
 
 
 def starting_page(request):
-    return render(request , "blog/index.html")
+    latest_post=Post.objects.all().order_by("-date")[:3]
+    return render(request , "blog/index.html" ,{
+        "posts":latest_post
+    })
 
 
 def all_posts(request):
-    return render(request , "blog/all-posts.html")
+    posts=Post.objects.all().order_by("-date")
+    return render(request , "blog/all-posts.html" , {
+        "posts":posts
+    })
 
 
 def single_post(request , slug):
-    return render(request , "blog/single-post.html")
+    identified_post=get_object_or_404(Post , slug=slug)
+    return render(request , "blog/single-post.html" ,{
+        "post": identified_post ,
+        "post_tags": identified_post.tags.all()
+    })
